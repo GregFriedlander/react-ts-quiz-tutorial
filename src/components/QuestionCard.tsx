b9248edd97caf3fa866f8f1dca43ccb3ebf6cd7f
Greg Friedlander <gregfriedlander@gmail.com>
2021-10-22 09:35:08 -0400

    react ts quiz tutorial init

---
+++ src/components/QuestionCard.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import { AnswerObject } from '../App';
+import { Wrapper, ButtonWrapper } from './QuestionCard.style';
+
+type Props = {
+  question: string;
+  answer: string[];
+  callback: (e: React.MouseEvent<HTMLButtonElement>) => void;
+  userAnswer: AnswerObject | undefined;
+  questionNr: number;
+  totalQuestions: number;
+}
+
+const QuestionCard: React.FC<Props> = (
+  {
+    question,
+    answer,
+    callback,
+    userAnswer,
+    questionNr,
+    totalQuestions
+  }) => (
+  <Wrapper>
+    <p className="number">
+      Question: {questionNr} / {totalQuestions}
+    </p>
+    <p dangerouslySetInnerHTML={{ __html: question }}></p>
+    <div>
+      {answer.map(answer => (
+        <ButtonWrapper
+          key={answer}
+          correct={userAnswer?.correctAnswer === answer}
+          userClicked={userAnswer?.answer === answer}
+        >
+          <button disabled={!!userAnswer} value={answer} onClick={callback}>
+            <span dangerouslySetInnerHTML={{ __html: answer }}></span>
+          </button>
+        </ButtonWrapper>
+      ))}
+    </div>
+  </Wrapper>
+)
+
+export default QuestionCard;
